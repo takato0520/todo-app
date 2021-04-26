090ec2cfd4eb86c9c01161f0dcf131c5a7e1bdbd
高戸良輔 <r.takato0520@gmail.com>
2021-04-26 21:34:20 +0900

    taskDetailのスケルトン作成

--- src/TaskDetail.jsx
+++ src/TaskDetail.jsx
@@ -1,5 +1,16 @@
 const TaskDetail = () => {
-    return (null)
+    return (
+        <>
+            <h1>タスク名</h1>
+            <div>期限</div>
+            <div>かかる時間</div>
+            <h2>タスクの詳細</h2>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <div>変更</div>
+            <div>削除</div>
+            <div>閉じる</div>
+        </>
+    )
 }
 
 export default TaskDetail
