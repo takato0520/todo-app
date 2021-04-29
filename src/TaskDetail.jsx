6bd5fd7960d2402ee15f060d3d21a8f29c2f32dc
高戸良輔 <r.takato0520@gmail.com>
2021-04-29 23:20:15 +0900

    テキストの削除ボタン、詳細画面を閉じるボタンの追加

--- src/TaskDetail.jsx
+++ src/TaskDetail.jsx
@@ -1,14 +1,37 @@
-const TaskDetail = () => {
+import { useState } from 'react'
+
+const TaskDetail = ({ history }) => {
+    const [text, setText] = useState()
+
+    const deleteText = (e) => {
+        e.preventDefault()
+        setText("")
+    }
+
+    const moveToRoom = (e) => {
+        e.preventDefault()
+        history.push("/")
+    }
+    console.log(text)
+
     return (
         <>
             <h1>タスク名</h1>
             <div>期限</div>
             <div>かかる時間</div>
             <h2>タスクの詳細</h2>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <div>変更</div>
-            <div>削除</div>
-            <div>閉じる</div>
+            <div>{text}</div>
+            <textarea
+                value={text}
+                onChange={
+                    e => {
+                        setText(e.target.value)
+                    }
+                }
+            />
+            <button>保存</button>
+            <button onClick={deleteText}>削除</button>
+            <button onClick={moveToRoom}>閉じる</button>
         </>
     )
 }
