79ea17d0444289d728b49a2cad1645beb66f651e
高戸良輔 <r.takato0520@gmail.com>
2021-05-10 21:43:00 +0900

    App.jsxをpullした

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Switch, Route, withRouter } from "react-router-dom"
 import Room from './Room'
 import Login from './Login'
 import SignUp from './SignUp'
 import TaskDetail from './TaskDetail'
 import TaskHistory from './TaskHistory'
 import Header from './Header'
+import Taskinput from './Taskinput/taskinput'
+// import  {useEffect, useState} from 'react'
+import firebase from './config/firebase'
+import 'firebase/firestore'
 
 function App() {
+  
+  // firestoreからdate取得するコードです
+ 
+
+  const getTasks = () => { 
+    firebase.firestore().collection('tasks')
+    .onSnapshot((snapshot) => {
+      const tasks = snapshot.docs.map(doc => {
+
+        return doc.data()
+      })
+      console.log(tasks)
+    });
+  }
+  // const [getTasks, setGetTasks] = useState()
+
+  // useEffect(() => {
+  //   firebase.firestore().collection('tasks')
+  //   .onSnapshot((snapshot) => {
+  //       const getTasks = snapshot.docs.map(doc => {
+
+  //           return doc.data()
+  //       })
+  //       console.log(getTasks)
+  //       setGetTasks(getTasks)
+  //   });
+  // }, [])  
+    
+  
+
+        
+
+
   return (
     <>
+      {/* <button　onClick= {getTasks}>取得</button> データが取得されているかの確認するボタンです。（テスト用） */}
       <Header />
-      <Router>
+      <withRouter>
         <Switch>
           <Route exact path='/' component={Room} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/signup' component={SignUp} />
-          <Route exact path='/taskDetail' component={TaskDetail} />
-          <Route exact path='/taskHistory' component={TaskHistory} />
+          <Route exact path='/taskDetail' component={TaskDetail} getTasks={getTasks} />
+          <Route exact path='/taskHistory' component={TaskHistory} getTasks={getTasks} />
         </Switch>
-      </Router>
+      </withRouter>
+      <Taskinput getTasks={getTasks} />
     </>
   )
 }
