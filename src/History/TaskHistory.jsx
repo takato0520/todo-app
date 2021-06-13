aa453e1dbf73d77f0d5735ee43f44832ed935495
高戸良輔 <r.takato0520@gmail.com>
2021-06-13 22:46:39 +0900

    履歴画面のスタイルを作成

--- src/History/TaskHistory.jsx
+++ src/History/TaskHistory.jsx
@@ -1,7 +1,9 @@
-import styled from 'styled-components'
-import { useState, useEffect } from 'react'
-import CompletedTask from './CompletedTask'
-
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import firebase from '../config/firebase'
+import 'firebase/firestore'
+import DeleteIcon from '@material-ui/icons/Delete';
+import dayjs from 'dayjs'
 
 const TaskHistory = ({ getTasks }) => {
 
@@ -15,20 +17,111 @@ const TaskHistory = ({ getTasks }) => {
     console.log(completedTasks)
     console.log(getTasks)
 
+    //メイン画面にtaskを再表示する処理
+    const restoreTask = (id) => {
+        firebase.firestore().collection('tasks').doc(id)
+            .update({
+                isCompleted: false
+            })
+    }
+
+    //履歴画面からtaskを削除する処理（DBから完全に消去する処理）
+    const deleteTask = (id) => {
+        firebase.firestore().collection('tasks').doc(id)
+            .delete()
+    }
+
     return (
         <>
-            <h1>履歴</h1>
+            <Title>履歴</Title>
+            <IndexBox>
+                <ItemTaskName>
+                    件名
+                    </ItemTaskName>
+                <ItemRequiredTime>
+                    所要時間
+                    </ItemRequiredTime>
+                <ItemDeadline>
+                    期日
+                    </ItemDeadline>
+            </IndexBox>
+
             <ul>
                 {
                     completedTasks.map((completedTask => {
                         return (
-                            <CompletedTask completedTask={completedTask} />
+                            <ItemBox>
+                                <ItemTaskName >{completedTask.taskName} </ItemTaskName>
+                                <ItemRequiredTime >{completedTask.requiredTime} min</ItemRequiredTime>
+                                <ItemDeadline >{dayjs(completedTask.deadline).format('YYYY/MM/DD HH:mm')}</ItemDeadline>
+                                <ItemDetail onClick={() => restoreTask(completedTask.id)}>復元</ItemDetail>
+                                <ItemDeleteIcon
+                                    onClick={() => deleteTask(completedTask.id)}
+                                    value="削除" />
+                            </ItemBox>
                         )
                     }))
                 }
-            </ul>
+            </ul >
         </>
     )
 }
 
+const Title = styled.div`
+height:72px;
+margin-left:20px;
+pointer:30px;
+display:flex;
+align-items:center;
+`
+
+//outputエリアのstyle
+const TaskWrap = styled.div`
+width: 100%;
+`
+const IndexBox = styled.div`
+display:flex;
+align-items:center;
+margin-top:30px;
+background-color:#00CC99;
+height:70px;
+`
+
+const ItemBox = styled.div`
+height:50px;
+display:flex;
+align-items:center;
+background-color:#CCFFFF;
+margin-top:5px;
+`
+
+const Item = styled.div`
+margin-left: 20px;
+`
+
+const ItemTaskName = styled(Item)`
+width:20%;
+`
+const ItemRequiredTime = styled(Item)`
+width:20%;
+`
+const ItemDeadline = styled(Item)`
+width:35%;
+`
+const ItemDetail = styled(Item)`
+width:50px;
+    :hover{
+        opacity:0.8;
+        color:#0066FF;
+    }
+`
+const ItemDeleteIcon = styled(DeleteIcon)`
+font-size:18px;
+cursor:pointer;
+    :hover{
+        opacity:0.8;
+        color:#0066FF;
+
+    }
+`
 export default TaskHistory
